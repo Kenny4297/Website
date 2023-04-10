7253082e75ea34d7e5802a877c3955a40d73011a
Kenny Cordero <GeckoB4I@gmail.com>
2023-04-10 16:36:31 -0500

    completed responsive banner

--- src/App.js
+++ src/App.js
@@ -19,5 +19,9 @@ const Container = styled.div``
 const Banner = styled.div`
   background: linear-gradient(159deg, rgb(45, 45, 58) 0%, rgb(43, 43, 53) 100%);
   height: 100vh;
+  @media(max-width: 640px) {
+    height: 100%;
+    padding-bottom: 1rem;
+  }
 `
 
