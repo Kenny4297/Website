88f6d2179d4319a19bcadc4fd58109b346f19d64
Kenny Cordero <GeckoB4I@gmail.com>
2023-04-10 17:40:09 -0500

    began working on the servies section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components'
 import Header from "./components/Banner/Header"
 import ProfComponent from './components/Banner/ProfComponent';
+import Services from './components/Service/Services'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <ProfComponent />
     </Banner>
+    <Services />
    </Container>
   );
 }
---
+++ src/components/Service/Services.jsx
@@ -0,0 +1,54 @@
+import { MdDesignServices } from "react-icons/md";
+import { FiCodesandbox } from "react-icons/fi";
+import { CgWebsite } from "react-icons/cg";
+import styled from 'styled-components'
+import Card from "./Card";
+
+const Services = () => {
+    return (
+        <Container>
+            <h4>My <span className='green'>services</span></h4>
+            <h1>What I Do</h1>
+            <Cards>
+                <Card
+                Icon={MdDesignServices} 
+                title={"Full Stack React developer"}
+                disc={"--Put description here--"}
+                />
+
+                <Card
+                Icon={FiCodesandbox} 
+                title={"UX/UI designer"}
+                disc={"--Put description here--"}
+                />
+
+                <Card
+                Icon={CgWebsite} 
+                title={"Backend Developer"}
+                disc={"--Put description here--"}
+                />
+            </Cards>
+        </Container>
+    )
+}
+
+export default Services;
+
+const Container = styled.div`
+    width: 80%;
+    max-width: 1280px;
+    margin: 0 auto;
+    padding: 3rem 0;
+    @media (max-width: 840px) {
+        width: 90%;
+    }
+    h1 {
+        padding-top: 1rem;
+    }
+`;
+const Cards = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+    margin-top: 4rem;
+    gap: 1rem;
+`;
