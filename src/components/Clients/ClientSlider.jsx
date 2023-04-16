cad7c773214c0714fa9c24a9739676fbf1d178c0
Kenny Cordero <GeckoB4I@gmail.com>
2023-04-16 09:08:05 -0500

    WIP: work on testimonials page

--- src/components/Clients/ClientSlider.jsx
+++ src/components/Clients/ClientSlider.jsx
@@ -8,8 +8,8 @@ const ClientSlider = (props) => {
     return (
         <Container>
             <Header>
-                <span><IoIosQuote/></span>
-                <span>
+                <span className="quote"><IoIosQuote/></span>
+                <span className = 'star'>
                     <AiOutlineStar/>
                     <AiOutlineStar/>
                     <AiOutlineStar/>
@@ -24,6 +24,7 @@ const ClientSlider = (props) => {
                 <img src={img_url} alt={name} />
                 <div className="details">
                     <h1>{name}</h1>
+                    <p>{position}</p>
                 </div>
             </Footer>
         </Container>
@@ -32,7 +33,47 @@ const ClientSlider = (props) => {
 
 export default ClientSlider;
 
-const Container = styled.div``;
-const Header = styled.div``;
-const Body = styled.p``
+const Container = styled.div`
+    background: linear-gradient(159deg, rgb(45, 45, 58) 0%, rgb(43, 43, 53) 100%);
+    padding: 0.5rem 1rem 1rem 1rem;
+    margin: 0 1rem;
+`;
+const Header = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
 
+    .quote {
+        font-size: 3rem;
+        color: #01be96;
+        opacity: 0.7;
+    }
+
+    .star {
+        color: #ffcd3c;
+        font-size:1.3rem;
+    }
+`;
+const Body = styled.p`
+    font-size: 0.8rem;
+    margin-bottom: 1.5rem;
+`;
+const Footer = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+        img {
+            width: 4rem;
+            border-radius: 50px;
+        }
+
+        h1 {
+            font-size: 1.2rem;
+            font-weight: 700;
+        }
+
+        p {
+            font-size: 0.8rem;
+            color: rgba(255,255,255,0.500);
+        }
+`
