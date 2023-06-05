79cc5f3e387535ba6f8882d8f952f5afa4d2b272
Kenny Cordero <geckob4i@gmail.com>
2023-06-05 16:13:14 -0500

    Add icons for projects

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -2,20 +2,16 @@ import styled from "styled-components";
 import React from 'react';
 import BeTethical from "../Banner/assets/BeTethicalUpdated.png";
 import weather from "../Banner/assets/weather-wizard.png";
-import quiz from '../Banner/assets/quizSuperCrop.png'
+import LearnC from '../Banner/assets/LearnC++.png'
+import { ReactOriginal, JavascriptPlain, TypescriptPlain, Html5PlainWordmark, Css3OriginalWordmark, NodejsOriginal, ExpressOriginal, MysqlOriginal }  from 'devicons-react';
 
 let data = [
+
     {
-        img: weather,
-        disc: "Explore the Weather Wizard, an intuitive app designed to keep you updated on the weather. Offering current conditions and a five-day forecast for any city worldwide, it also provides local time information. Tailored to provide an engaging user experience, it automatically runs the data for the last city you checked upon your return.",
-        title: "Weather-Wizard",
-        deployment: "https://weather-wizard4297.herokuapp.com/",
-    },
-    {
-        img: quiz,
-        disc: "Take on this riveting JavaScript quiz, testing your skills with five questions in 60 seconds, where each wrong answer deducts 10 seconds, yet high scorers earn the honor of landing on our high scores board.",
-        title: "JavaScript Quiz",
-        deployment: "https://kenny4297.github.io/JavaScript-Quiz/",
+        img: LearnC,
+        disc: `"Learn C++" is an innovative, hands-on learning platform that focuses on practicality, commencing with real-world projects and dissecting them to teach users the fundamentals of C++. The platform engages users through a combination of interactive lessons, quizzes, and challenges, effectively fostering a deep understanding of C++ basics.`,
+        title: "Learn C++",
+        deployment: "https://learn-c-plus-plus.netlify.app/",
     },
     {
         img: BeTethical,
@@ -23,17 +19,51 @@ let data = [
         title: "Be Tethical!",
         deployment: "https://be-tethical-production.up.railway.app/",
     },
+    {
+        img: weather,
+        disc: "Explore the Weather Wizard, an intuitive app designed to keep you updated on the weather. Offering current conditions and a five-day forecast for any city worldwide, it also provides local time information. Tailored to provide an engaging user experience, it automatically runs the data for the last city you checked upon your return.",
+        title: "Weather-Wizard",
+        deployment: "https://weather-wizard4297.herokuapp.com/",
+    },
 ];
 
 const Project = () => {
     const openInNewTab = true;
+
+    const logos = {
+        0: [{component: ReactOriginal }, 
+            {component: TypescriptPlain, color: "#007ACC"}, 
+            {component: Html5PlainWordmark, color: "#E34F26"}, 
+            {component: Css3OriginalWordmark}],
+
+        1: [{component: JavascriptPlain, color: "#F7DF1E"},
+            {component: Html5PlainWordmark, color: "#E34F26"},
+            {component: Css3OriginalWordmark},
+            {component: NodejsOriginal},
+            {component: ExpressOriginal, color: "red"},
+            {component: MysqlOriginal, color: "#000"}],
+
+
+        2: [{component: ReactOriginal, color: "#007ACC"}, 
+            {component: TypescriptPlain, color: "#007ACC"},
+            {component: Html5PlainWordmark, color: "#E34F26"}, 
+            {component: Css3OriginalWordmark}],
+    };
     return (
         <>
             {data.map((item, index) => (
                 <Container className="project" key={index}>
                     <ContentContainer>
                         <ImgContainer isFirst={index === 0} isSecond={index === 1} isThird={index === 2}>
-                            <img src={item.img} alt="project" />
+                            <a
+                                href={item.deployment}
+                                target={openInNewTab ? "_blank" : "_self"}
+                                rel="noreferrer"
+                                aria-label="Live Site"
+                            >
+                                <img src={item.img} alt="project" />
+                            </a>
+                            
                         </ImgContainer>
 
                         <DescContainer >
@@ -41,14 +71,12 @@ const Project = () => {
                             <p>
                                 {item.disc}
                             </p>
-                            <a
-                                href={item.deployment}
-                                target={openInNewTab ? "_blank" : "_self"}
-                                rel="noreferrer"
-                                aria-label="Live Site"
-                            >
-                                Live Site
-                            </a>
+                            <IconSection style={{paddingTop:'1rem'}}>
+                                {logos[index]?.map((logo, i) => {
+                                    const LogoComponent = logo.component;
+                                    return <LogoComponent key={i} color={logo.color} size={32.5} />
+                                })}
+                            </IconSection>
                         </DescContainer>
                     </ContentContainer>
                 </Container>
@@ -58,6 +86,10 @@ const Project = () => {
 };
 
 export default Project;
+
+const IconSection = styled.div`
+    padding-top: 1rem;
+`
 
 const ContentContainer = styled.div`
     display: flex;
@@ -75,10 +107,15 @@ const ImgContainer = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
+    /* border: 2px solid green; */
     img {
         width: 30rem;
-        height: 100%;
-        border: 3px solid #4e5156;
+        /* width: 70%; */
+        height: 17.2rem;
+        /* height: 100%; */
+        outline: 3px solid #4e5156;
+        position: relative;
+        top:.105rem;
         &:hover {
             transform: scale(1.05);
         }
@@ -100,7 +137,7 @@ const DescContainer = styled.div`
     max-width: 35rem;
     h3 {
         font-size: 1.75rem;
-        padding-top: .5rem;
+        /* padding-top: .5rem; */
     }
     p {
         width: 90%;
@@ -115,6 +152,8 @@ const DescContainer = styled.div`
         font-size: 1.5rem;
         color: #70CFFF;
         font-weight: bold;
+        height: 40rem;
+        border: 2px solid green;
         @media (max-width: 405px) {
             padding-bottom: 2rem;
         }
