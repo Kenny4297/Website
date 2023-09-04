2da16024e8970d5f47dfa43db1f253f3f308d341
Kenny Cordero <geckob4i@gmail.com>
2023-09-04 09:16:53 -0500

    Convert Profil Pic from png to webp for better loading time

--- src/components/Banner/ProfComponent.jsx
+++ src/components/Banner/ProfComponent.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled, { keyframes } from 'styled-components';
 import { FaLinkedinIn } from 'react-icons/fa'
 import { FaGithub } from 'react-icons/fa'
-import ProfNoBack from './assets/ProfNoBack.png'
+import ProfNoBack from './assets/ProfNoBack.webp'
 
 
 const ProfComponent = () => {
